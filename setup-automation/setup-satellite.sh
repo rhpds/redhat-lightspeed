e018d99b501f27859c2b62b73d8d8554c71b85d4
Scaffolder <scaffolder@backstage.io>
2026-09-22 12:28:26 +0000

    Import content from https://github.com/rhpds/zt-lb1187-hands-on-with-lightspeed-in-satellite

---
+++ setup-automation/setup-satellite.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd /root
+
+tee ~/config.yml << EOF
+---
+- name: Configure Satellite 6.18
+  hosts: localhost
+  remote_user: root
+
+  tasks:
+  - name: "Create an activation key."
+    redhat.satellite.activation_key:
+      username: "admin"
+      password: "bc31c9a6-9ff0-11ec-9587-00155d1b0702"
+      server_url: "https://satellite.lab"
+      name: "RHEL10"
+      content_view: "Default Organization View"
+      organization: "Acme Org"
+      lifecycle_environment: "Library"
+      content_overrides:
+          - label: satellite-client-6-for-rhel-10-x86_64-rpms
+            override: enabled
+EOF
+
+ansible-playbook config.yml
